bf04960b3334bd264aaea6d438bf56b7b4e9280f
Vaibhav Gupta <kacxdak@gmail.com>
2013-03-30 15:18:24 -0500

    adding main.h

---
+++ game/main.h
@@ -0,0 +1,21 @@
+//=================================
+// include guard
+#ifndef __MAIN_H_INCLUDED__
+#define __MAIN_H_INCLUDED__
+//=================================
+
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+void usage_err(string );
+
+//=================================
+// end guard
+#endif
+//=================================
